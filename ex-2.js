4a435a7e75248afdb15284ef4edd9c2fec7fb23f
Pongsakon <charlee2543@hotmail.com>
2025-02-26 13:49:35 +0700

    feat: answer true/false

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,10 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //true
+null == undefined; //true
+// console.log('null == undefined: ', null == undefined);
+10 == "10"; //true
+[10] == 10; //true
+// console.log('[10] == 10: ', [10] == 10);
+[1, 2] == "1,2"; //true
+// console.log('[1,2] == "1,2": ', [1,2] == "1,2");
